65b111cc3d7b71aa0394ffe123189562dcc4162a
el-mubarok <mubarok13@gmail.com>
2015-04-25 15:47:16 +0700

    Oyi

---
+++ src/INstanceVar.java
@@ -0,0 +1,60 @@
+
+public class INstanceVar {
+
+	private String KondLantai, KonDinding, KonAtap, KonPintu, KonJendela, SirkulUdara;
+	private double NilaiCahaya, Kelembapan, Suhu;
+	public String getKondLantai() {
+		return KondLantai;
+	}
+	public void setKondLantai(String kondLantai) {
+		KondLantai = kondLantai;
+	}
+	public String getKonDinding() {
+		return KonDinding;
+	}
+	public void setKonDinding(String konDinding) {
+		KonDinding = konDinding;
+	}
+	public String getKonAtap() {
+		return KonAtap;
+	}
+	public void setKonAtap(String konAtap) {
+		KonAtap = konAtap;
+	}
+	public String getKonPintu() {
+		return KonPintu;
+	}
+	public void setKonPintu(String konPintu) {
+		KonPintu = konPintu;
+	}
+	public String getKonJendela() {
+		return KonJendela;
+	}
+	public void setKonJendela(String konJendela) {
+		KonJendela = konJendela;
+	}
+	public String getSirkulUdara() {
+		return SirkulUdara;
+	}
+	public void setSirkulUdara(String sirkulUdara) {
+		SirkulUdara = sirkulUdara;
+	}
+	public double getNilaiCahaya() {
+		return NilaiCahaya;
+	}
+	public void setNilaiCahaya(double nilaiCahaya) {
+		NilaiCahaya = nilaiCahaya;
+	}
+	public double getKelembapan() {
+		return Kelembapan;
+	}
+	public void setKelembapan(double kelembapan) {
+		Kelembapan = kelembapan;
+	}
+	public double getSuhu() {
+		return Suhu;
+	}
+	public void setSuhu(double suhu) {
+		Suhu = suhu;
+	}
+}
